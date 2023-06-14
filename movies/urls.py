c760de253ab781b239dd80ea66f96f54948ba2db
day26 <day26@i.ua>
2023-06-14 23:18:21 +0300

    include, extends

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# 1
 urlpatterns = [
     path("", views.MoviesView.as_view(), name="main"),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail")
